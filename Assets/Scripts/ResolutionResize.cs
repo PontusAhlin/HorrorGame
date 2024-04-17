c625d4c95fddafafb6c05ce49964649f5b5f66fd
alin <doublea654@gmail.com>
2024-04-17 15:41:41 +0200

    made a scene that needs to be loaded first to adjust texture

--- Assets/Scripts/ResolutionResize.cs
+++ Assets/Scripts/ResolutionResize.cs
@@ -1,24 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ResolutionResize : MonoBehaviour
 {
     [SerializeField] RenderTexture renderTexture;
+    [SerializeField] int DivideResolutionBy; //1 IS NONE
     // Start is called before the first frame update
 
-    
-    void Start()
+    void Start() //WE NEED TO MAKE THIS RUN BEFORE RUNTIME!!!
     {
-        Resize(renderTexture, Screen.width, Screen.height);
+        ResizeRenderTexture(renderTexture, Screen.width, Screen.height); //runs our resize function for our screen 
+        SceneManager.LoadScene("ButtonScene");
     }
+
     
-    void Resize(RenderTexture renderTexture, int width, int height) {
+    void ResizeRenderTexture(RenderTexture renderTexture, int width, int height) {
          if (renderTexture) {
             renderTexture.Release();
-            renderTexture.width = width/2;
-            renderTexture.height = height/2; 
+            renderTexture.width = width/DivideResolutionBy;
+            renderTexture.height = height/DivideResolutionBy; 
         }
     }
+
 
 }
