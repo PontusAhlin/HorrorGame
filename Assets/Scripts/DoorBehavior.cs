aa0b724965a385b2aa9a57a746eacca2e4c3b391
Sai.C <saitejas.chintapalli.uni@gmail.com>
2024-04-18 08:11:56 +0200

    door can open both ways

--- Assets/Scripts/DoorBehavior.cs
+++ Assets/Scripts/DoorBehavior.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class DoorBehavior : MonoBehaviour
+public class DoorBehavior1 : MonoBehaviour
 {
-    public GameObject doorOpened, doorClosed, inTrigger, outTrigger;
-    bool DoorIsClosed = true;
+    public GameObject doorTrigger, openDoor, closedDoor;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     private void OnTriggerEnter(Collider other) {
-        if(other.gameObject.tag == "Ball"){
-        Debug.Log("door should open now");
-        doorClosed.SetActive(false);
-        doorOpened.SetActive(true);
+        if(other.gameObject.CompareTag("Ball")){
+            if(closedDoor.activeInHierarchy == true){
+                    Debug.Log("door opens");
+                    closedDoor.SetActive(false);
+                    openDoor.SetActive(true);
+                }
+            Debug.Log("door should open now");
         }
         Debug.Log("end or door opening bit");
     }
@@ -25,8 +26,8 @@ public class DoorBehavior : MonoBehaviour
     private void OnTriggerExit(Collider other){
         Debug.Log("door should close");
         if(other.gameObject.tag == "Ball"){
-        doorClosed.SetActive(true);
-        doorOpened.SetActive(false);
+            closedDoor.SetActive(true);
+            openDoor.SetActive(false);
         }
     }        
     // Update is called once per frame
