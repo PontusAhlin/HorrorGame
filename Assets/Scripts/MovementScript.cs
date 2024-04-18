02e6f88f0b4980fcf3e188b58373ecb2926e44ca
Fridh <william.fridh@gmail.com>
2024-04-18 11:03:26 +0200

    Refined the movement-script to automate configuration.

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -2,28 +2,42 @@
     * This script is attached to the player object and is responsible for moving the player in the direction of the camera.
     * The player moves in the direction of the camera's forward vector, rotated by the camera's y rotation.
     * This allows the player to move in the direction they are looking.
+    * 
+    * Authors: William Fridh, Alin-Cristian Serban
     */
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
-    // Variables set in Unity Editor
-    public Transform cam;
+    // Public variables set in Unity Editor
+    private Transform camTransform;
     public float speed = 1f;
+    private Gamepad gamepad; // We're using INPUTSYSTEM, configured it to imitate gamepad buttons touchscreens
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamepad = Gamepad.current;                              // Select current gamepad
+        Camera childCam = GetComponentInChildren<Camera>();     // Find child camera
+        if (childCam != null) {
+            camTransform = childCam.transform;                  // Get the camera's transform
+        } else {
+            Debug.LogError("No child Camera found");
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        var gamepad = Gamepad.current; //we are using INPUTSYSTEM, and i've configured it to imitate gamepad buttons for our touchscreen
+        //var gamepad = Gamepad.current; // We're using INPUTSYSTEM, configured it to imitate gamepad buttons touchscreens
         if (gamepad == null)
-            return; // No gamepad connected.
+            return; // No gamepad connected
 
-        if (gamepad.buttonNorth.isPressed) //north is our current movement button. so, this means "if this is pressed".
-        {
-        float targetAngle = cam.eulerAngles.y;                                      // Get the camera's y rotation
-        Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;  // Rotate the forward vector by the camera's y rotation
-        transform.position += moveDir.normalized * speed * Time.deltaTime;          // Move the player in the direction of the rotated forward vector
+        if (gamepad.buttonNorth.isPressed) {                                            // "butttonNorth" is our current movement button
+            float targetAngle = camTransform.eulerAngles.y;                             // Get the camera's y rotation
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;  // Rotate the forward vector by the camera's y rotation
+            transform.position += moveDir.normalized * speed * Time.deltaTime;          // Move the player in the direction of the rotated forward vector
         }
     }
 }
