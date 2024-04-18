e1027a70dfc865d72fa433bb608c5823b8b2152e
alin <doublea654@gmail.com>
2024-04-18 10:31:00 +0200

    default changed to 300 pixels wide

--- Assets/Scripts/ResolutionResize.cs
+++ Assets/Scripts/ResolutionResize.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 public class ResolutionResize : MonoBehaviour
 {
     [SerializeField] RenderTexture renderTexture; //this is needed to actually fetch the render texture
-    [SerializeField] int TargetWidthInPixels = 400; //1 IS NONE
+    [SerializeField] int TargetWidthInPixels = 300; //1 IS NONE
     [SerializeField] bool IsPixelationOn = true;
     // Start is called before the first frame update
 
