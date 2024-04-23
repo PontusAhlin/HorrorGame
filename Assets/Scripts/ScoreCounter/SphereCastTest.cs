ba56a7275c7cfad869d124fe6dcd15038125a18a
PontusAhlin <pontus.ahlin@live.se>
2024-04-23 11:47:08 +0200

    Working monster detection system with added score-counter

--- Assets/Scripts/ScoreCounter/SphereCastTest.cs
+++ Assets/Scripts/ScoreCounter/SphereCastTest.cs
@@ -4,36 +4,48 @@ using UnityEngine;
 
 public class SphereCastTest : MonoBehaviour
 {
+    /* //Added for debugging
+    [Tooltip("List of objects which the raycast sphere detects ")]
     public List<GameObject> currentHitObjects = new List<GameObject>();
-
+    */
+    
+    [Tooltip("Connect this to your main camera")]
     public Camera camera;
     
-    public float sphereRadius; 
-    public float maxDistance;
-    public float minDistance;
+    [Tooltip("Reference from 'PlayerScore.cs', add the monster gameObject to this")]
+    public MonsterGenerateViewers monsterGenerateViewers;
 
+    private bool monsterDetected;
+
+
+    //Init of detection components
+    [Tooltip("Radius of the raycasted sphere")]
+    public float sphereRadius; 
+    [Tooltip("How far the raycast sphere should go")]
+    public float maxDistance;
+    [Tooltip("The 'offset' from the player forward which the sphere will start from  ")]
+    public float minDistance;
     private Vector3 sphereCastOffset;
     private Vector3 playerDirection;
     private float currentHitDistance;
+    [Tooltip("DON'T TOUCH(BUT IF YOU DO SET IT BACK TO 'Default')")]
     public LayerMask layerMask;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate(){
         //Sets the players position and direction continuisly where the player looks 
         sphereCastOffset = transform.position + transform.forward * minDistance;
         playerDirection = transform.forward;
-        //Clears the gameObject list each frame
-        currentHitObjects.Clear();
+        
+        //Added to reset if the monster isn't in the field of view
+        monsterGenerateViewers.inFieldOfView = false;
 
+        /*
+        // Clears the gameObject list each frame(Used for debugging)
+            currentHitObjects.Clear();
+        */
+        
         //This line gives us an array with everything our raycast sphere hits 
         RaycastHit[] hits = Physics.SphereCastAll(sphereCastOffset, sphereRadius, playerDirection, maxDistance, layerMask, QueryTriggerInteraction.UseGlobal);
 
@@ -53,17 +65,23 @@ public class SphereCastTest : MonoBehaviour
                 // Debug ray to see if we hit something between player and monster.
                 Debug.DrawRay(transform.position, monsterHitDirection * (distanceBetween - 0.1f), Color.red);
 
-                //This makes a new separate raycast towards the monster, if there is something colliding with the raycast it doesn't register the monster 
+                //This makes a new separate raycast towards the monster, if there is something colliding with the raycast it doesn't register the monster.
                 if(Physics.Raycast(transform.position, monsterHitDirection, out hitMonster , distanceBetween - 0.1f) == false){
-                    currentHitObjects.Add(hit.transform.gameObject);
-                    print("SHIT A MONSTER");
-
-                    //Here we should add the points 
-
+                    /* //Used for debugging
+                        currentHitObjects.Add(hit.transform.gameObject);
+                    */
+                    
+                    //If we're in this if statement it means that we can see the monster
+                    //and each frame the monster is detected a view is added
+                    print("TRUE");
+                    monsterDetected = true;
+                    monsterGenerateViewers.inFieldOfView = true;
                 }
             }
         }
     }
+
+    
 
     
     //Debugging by creating the raycast sphere 
