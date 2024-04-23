fae81dfabd1a25e39952f6526e7d2da88783fa1c
Sai.C <saitejas.chintapalli.uni@gmail.com>
2024-04-23 16:41:07 +0200

    fixed shit. escape displays score

--- Assets/Scripts/EscapeSceneCode.cs
+++ Assets/Scripts/EscapeSceneCode.cs
@@ -2,9 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EscapeSceneCode : MonoBehaviour
-{    
+{   
+    [SerializeField]
+    Text scoreText;
+
+    [SerializeField]
+    int score = 1337;
+    void Start(){
+        scoreText.text = "SCORE: " + score.ToString();
+    }
+    void Update(){
+                scoreText.text = "SCORE: " + score.ToString();
+    }
     public void BackToMenu(){
         Debug.Log("pls work");
         SceneManager.LoadScene("MainMenu");
