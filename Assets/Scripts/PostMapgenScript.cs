91c4199e717d8e919aaed96e8540355fd3438836
Sai.C <saitejas.chintapalli.uni@gmail.com>
2024-04-25 14:26:01 +0200

    made escape door generate on random map

--- Assets/Scripts/PostMapgenScript.cs
+++ Assets/Scripts/PostMapgenScript.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 /*BY: ALIN
@@ -10,10 +11,12 @@ other scripts from here too.
 */
 
 public class PostMapgenScript : MonoBehaviour{
+    public GameObject SaiBallTester; //can be removed just for testing
     public GameObject escapeDoor;
     public GameObject RandomMapParent;
     public RandomMapHandler randscript;
     private int mapWidth, mapHeight, roomSize, escapeX, escapeZ, searchOffset;
+    string direction;
     public void Main()
     {
         mapWidth = gameObject.GetComponent<RandomMapHandler>().MapWidth;
@@ -24,38 +27,59 @@ public class PostMapgenScript : MonoBehaviour{
         while(!found){
             //down to up on left side
             for(int z = searchOffset; z<mapHeight - searchOffset; z++){
-                if(randscript.gridHandler[searchOffset, z] != RandomMapHandler.Grid.EMPTY){
+                if(randscript.gridHandler[searchOffset, z] != RandomMapHandler.Grid.EMPTY && !found){
                     escapeX = searchOffset;
                     escapeZ = z;
                     found = true;
+                    direction = "left";
                 }
             }
             //left to right on top side
             for(int x = searchOffset; x<mapWidth - searchOffset; x++){
-                if(randscript.gridHandler[x, mapHeight - searchOffset - 1] != RandomMapHandler.Grid.EMPTY){
+                if(randscript.gridHandler[x, mapHeight - searchOffset - 1] != RandomMapHandler.Grid.EMPTY && !found){
                     escapeX = x;
                     escapeZ = mapHeight - searchOffset - 1;
                     found = true;
+                    direction = "up";
                 }
             }
             //down to up on right side
             for(int z = searchOffset; z<mapHeight - searchOffset; z++){
-                if(randscript.gridHandler[mapWidth - searchOffset - 1, z] != RandomMapHandler.Grid.EMPTY){
+                if(randscript.gridHandler[mapWidth - searchOffset - 1, z] != RandomMapHandler.Grid.EMPTY && !found){
                     escapeX = mapWidth - searchOffset - 1;
                     escapeZ = z;
                     found = true;
+                    direction = "right";
                 }
             }
             //left to right on bottom side
             for(int x = searchOffset; x<mapWidth - searchOffset; x++){
-                if(randscript.gridHandler[x, searchOffset] != RandomMapHandler.Grid.EMPTY){
+                if(randscript.gridHandler[x, searchOffset] != RandomMapHandler.Grid.EMPTY && !found){
                     escapeX = x;
                     escapeZ = searchOffset;
                     found = true;
+                    direction = "down";
                 }
             }
             searchOffset++;    
         }
         Debug.Log("the door coords: (" + escapeX + ", " + escapeZ + ")");
+        Instantiate(SaiBallTester, new Vector3(escapeX * roomSize, 0, escapeZ * roomSize), Quaternion.identity); //this can be deleted
+        if(direction == "left"){
+            Instantiate(escapeDoor, new Vector3(escapeX * roomSize - roomSize/2 + 0.31f, 0, escapeZ * roomSize),
+            Quaternion.Euler(0, 90, 0)); 
+        }
+        else if(direction == "up"){
+            Instantiate(escapeDoor, new Vector3(escapeX * roomSize, 0, escapeZ * roomSize + roomSize/2 - 0.31f),
+            Quaternion.Euler(0, 180, 0));
+        }
+        else if(direction == "right"){
+            Instantiate(escapeDoor, new Vector3(escapeX * roomSize + roomSize/2 - 0.31f, 0, escapeZ * roomSize),
+            Quaternion.Euler(0, -90, 0));
+        }
+        else if(direction == "down"){
+            Instantiate(escapeDoor, new Vector3(escapeX * roomSize, 0, escapeZ * roomSize - roomSize/2 + 0.31f),
+            Quaternion.identity);
+        }
     }   
 }
