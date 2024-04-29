0a8148bd1f176c1991b594dc7c35d751da1971ea
alin <doublea654@gmail.com>
2024-04-29 15:10:51 +0200

    terrorball progress

--- Assets/Scripts/PrefabStuff/DoorTrigger.cs
+++ Assets/Scripts/PrefabStuff/DoorTrigger.cs
@@ -10,7 +10,7 @@ public class DoorTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other) {
         Debug.Log("door should open");
-        if(other.CompareTag("Ball") || other.CompareTag("Player") || other.CompareTag("Monster")){
+        if(other.CompareTag("Ball") || other.CompareTag("Player") || other.CompareTag("Monster") || other.CompareTag("SpecialMonster")){
         Debug.Log("door should open 2");
             if (!Door.isOpen){
                 Debug.Log("should trigger");
@@ -20,7 +20,7 @@ public class DoorTrigger : MonoBehaviour
     }
 
     private void OnTriggerExit(Collider other) {
-        if(other.CompareTag("Ball") || other.CompareTag("Player") || other.CompareTag("Monster")){
+        if(other.CompareTag("Ball") || other.CompareTag("Player") || other.CompareTag("Monster") || other.CompareTag("SpecialMonster")){
             if (Door.isOpen){
                 Debug.Log("should also trigger");
                 Door.Close();
