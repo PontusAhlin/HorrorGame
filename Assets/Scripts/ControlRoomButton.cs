1620dc735e5a59360360fd7f9736701eb8aaf3be
alin <doublea654@gmail.com>
2024-04-30 10:16:08 +0200

    fix successful - made it enable the trigger

--- Assets/Scripts/ControlRoomButton.cs
+++ Assets/Scripts/ControlRoomButton.cs
@@ -8,21 +8,26 @@ public class ControlRoomButton : MonoBehaviour
     public AudioSource pressedSound;    
     public AudioSource releasedSound;
 
-    void OnTriggerEnter(){
-        pressedSound.pitch = 1;
-        pressedSound.Play();
-        Debug.Log("trigger entered");
-        GameObject EscapeDoorTrigger = GameObject.Find("/justDoor(withCam)(Clone)/EscapeDoorTrigger");
-        Debug.Log(EscapeDoorTrigger);
-        EscapeDoorTrigger.SetActive(true);
-        //onPressed.Invoke();
-
+    void OnTriggerEnter(Collider other){
+        if (other.tag == "Player")
+        {
+            pressedSound.pitch = 1;
+            pressedSound.Play();
+            Debug.Log("trigger entered");
+            GameObject EscapeDoorTrigger = GameObject.Find("/justDoor(withCam)(Clone)/EscapeDoorTrigger");
+            Debug.Log(EscapeDoorTrigger);
+            EscapeDoorTrigger.GetComponent<Collider>().isTrigger = true;
+            //onPressed.Invoke();
+        }
 
     }
 
-    void OnTriggerExit(){
-        releasedSound.pitch = Random.Range(1.1f, 1.2f);
-        releasedSound.Play();
-        //onReleased.Invoke();
+    void OnTriggerExit(Collider other){
+        if (other.tag == "Player")
+        {
+            releasedSound.pitch = Random.Range(1.1f, 1.2f);
+            releasedSound.Play();
+            //onReleased.Invoke();
+        }
     }
 }
