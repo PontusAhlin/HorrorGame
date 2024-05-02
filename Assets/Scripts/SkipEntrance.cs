e4ffc09e817c2c8b2c826cbb936fea09a6f055b4
Arnob <arnobds5@gmail.com>
2024-05-02 10:09:02 +0200

    Context and skip button added

---
+++ Assets/Scripts/SkipEntrance.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SkipEntrance : MonoBehaviour
+{
+    // Reference to the Canvas component
+    public Canvas canvas;
+
+    // Function to disable the Canvas
+    public void DisableCanvas()
+    {
+        if(canvas != null)
+        {
+            canvas.enabled = false;
+        }
+        else
+        {
+            Debug.LogError("Canvas reference not set!");
+        }
+    }
+}
