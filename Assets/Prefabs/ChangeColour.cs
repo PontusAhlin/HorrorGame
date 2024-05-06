15fffc8b14a549ae3cbb9232587870e37e2ae3fe
Moritz <moritzgruss@hotmail.se>
2024-05-06 16:10:33 +0200

    Added script for chaning colour to random at start

--- Assets/Prefabs/ChangeColour.cs
+++ Assets/Prefabs/ChangeColour.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ChangeColour : MonoBehaviour
 {
     // SkinnedMeshRenderer is the component of the monster containing its materials
@@ -9,7 +10,28 @@ public class ChangeColour : MonoBehaviour
     private Material[] materials; // list of Materials applied to monster
     public Material[] materialsPossibleForUse; // list of Materials which we can to apply to monster
     private List<Material> materialsInUse = new List<Material>(); // Array of length 2 which contain the two current materials in use
+    Color cyanC = new Color(0f, 1f, 1f, 1f);
+    Color blackC = new Color(0f, 0f, 0f, 1f);
+    Color blueC = new Color(0f, 0f, 1f, 1f);
+    Color grayC = new Color(0.5f, 0.5f, 0.5f, 1f);
+    Color magentaC = new Color(1f, 0f, 1f, 1f);
+
+    private class ColorVar 
+        {
+            public Color color;
+            public string name;
+
+            public ColorVar(Color color, string name) 
+            {
+                this.color = color;
+                this.name = name;
+            }
+        }
+
+    List<ColorVar> colorPallet  = new List<ColorVar>();
     
+    
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,20 +40,32 @@ public class ChangeColour : MonoBehaviour
         skinnedMeshRenderer = GetComponentsInChildren<SkinnedMeshRenderer>();
         // extracts the materials into an array of type Material 
         materials = skinnedMeshRenderer[0].materials;
-        
-        // Adds the first element of the materialsPossibleForUse array to materialsInUse
-        materialsInUse.Add(materialsPossibleForUse[0]);
-        // Adds the second element of the materialsPossibleForUse array to materialsInUse
-        materialsInUse.Add(materialsPossibleForUse[1]);
+        // Makes instances of different colours
+        ColorVar cyan = new ColorVar(cyanC, "cyan");
+        ColorVar black = new ColorVar(blackC, "black");
+        ColorVar blue = new ColorVar(blueC, "blue");
+        ColorVar grey = new ColorVar(grayC, "grey");
+        ColorVar magenta = new ColorVar(magentaC, "magenta");
+        // Adds all instances of ColorVar to colorPallet
+        colorPallet.Add(cyan);
+        colorPallet.Add(black);
+        colorPallet.Add(blue);
+        colorPallet.Add(grey);
+        colorPallet.Add(magenta);
 
-        // applies the materials in materialsInUse to the monster
-        skinnedMeshRenderer[0].SetMaterials(materialsInUse);
+        
+
+        // Generate a random index within the range of the list
+        int randomIndex = Random.Range(0, colorPallet.Count);
+
+        materials[0].SetColor("_OutlineColor", colorPallet[randomIndex].color);
+        materials[1].SetColor("_OutlineColor", colorPallet[randomIndex].color);
 
         
         
     }
 
-    
 
-    
 }
+
+
