fcd1d927e2a77e8f63e830e396484f92153bcfdb
Moritz <moritzgruss@hotmail.se>
2024-05-06 15:06:35 +0200

    Function for chaning material

--- Assets/Prefabs/ChangeColour.cs
+++ Assets/Prefabs/ChangeColour.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class ChangeColour : MonoBehaviour
 {
-
-    public Material bodyColor;
+    // SkinnedMeshRenderer is the component of the monster containing its materials
+    public SkinnedMeshRenderer[] skinnedMeshRenderer;
+    private Material[] materials; // list of Materials applied to monster
+    public Material[] materialsPossibleForUse; // list of Materials which we can to apply to monster
+    private List<Material> materialsInUse = new List<Material>(); // Array of length 2 which contain the two current materials in use
     
 
     // Start is called before the first frame update
     void Start()
     {
-        setColor(Color.red);
-    }
+        // gets skinned Mesh renderer component from child object "Ch11"
+        skinnedMeshRenderer = GetComponentsInChildren<SkinnedMeshRenderer>();
+        // extracts the materials into an array of type Material 
+        materials = skinnedMeshRenderer[0].materials;
+        
+        // Adds the first element of the materialsPossibleForUse array to materialsInUse
+        materialsInUse.Add(materialsPossibleForUse[0]);
+        // Adds the second element of the materialsPossibleForUse array to materialsInUse
+        materialsInUse.Add(materialsPossibleForUse[1]);
 
-    // Update is called once per frame
-    void Update()
-    {
+        // applies the materials in materialsInUse to the monster
+        skinnedMeshRenderer[0].SetMaterials(materialsInUse);
+
+        
         
     }
 
-    public void setColor(Color c) 
-    {
-        bodyColor.color = c;
-    }
+    
+
+    
 }
