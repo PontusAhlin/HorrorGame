b943332c47f6076bcf5b944050cca18cebbfdeb9
PontusAhlin <pontus.ahlin@live.se>
2024-05-14 09:33:34 +0200

    Fixed entrance scene being stuck

--- Assets/Scripts/SkipEntrance.cs
+++ Assets/Scripts/SkipEntrance.cs
@@ -1,16 +1,38 @@
+/**
+    * A simple script to jump into the entrance scene from the tutorial 
+    * Authors: Arnob Dey Sarker, Pontus Åhlin
+*/
+
 using UnityEngine;
 
 public class SkipEntrance : MonoBehaviour
 {
     // Reference to the Canvas component
     public Canvas canvas;
-
+    private float skipTimer;
     // Function to disable the Canvas
+
+    void Start(){
+        skipTimer = 38.5f;
+    }
+
+
+    void FixedUpdate(){
+        print(skipTimer);
+
+        if(skipTimer < 1f){
+            DisableCanvas();
+        }
+        skipTimer -= Time.deltaTime;
+    }
+
     public void DisableCanvas()
     {
+
         if(canvas != null)
         {
             canvas.enabled = false;
+        
         }
         else
         {
